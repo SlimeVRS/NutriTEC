e0169763ecaa3e66e03e3fbfa4ae6ad5c0be2846
Dr.Garcia <48070831+traznar@users.noreply.github.com>
2021-11-05 17:07:29 -0600

    Configuracion inicial

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/home/login/login.component';
+import { RegisterComponent } from './components/home/register/register.component';
+import { NavbarComponent } from './components/home/navbar/navbar.component';
+import { ClientComponent } from './components/client/client.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ClientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ToastrModule.forRoot(),
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'singup', component: RegisterComponent}, 
+      {path: 'login', component: LoginComponent}, 
+      { path: '**', redirectTo: 'login', pathMatch: 'full' },
+      
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
