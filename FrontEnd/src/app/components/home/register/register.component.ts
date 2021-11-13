15252eab783ce278f42300cfb940eb7e07ed1af9
03-Kevin <kejorola@gmail.com>
2021-11-13 13:08:36 -0600

    Login y registrar cliente
    Solo hace falta cambiar ingreso de fecha.

--- FrontEnd/src/app/components/home/register/register.component.ts
+++ FrontEnd/src/app/components/home/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  form: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
