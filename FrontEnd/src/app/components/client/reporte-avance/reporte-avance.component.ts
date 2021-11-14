9ff484f0006e6920eabc4c52090424940397f1fc
03-Kevin <kejorola@gmail.com>
2021-11-14 13:17:19 -0600

    Reporte de avance
    Vista html

--- FrontEnd/src/app/components/client/reporte-avance/reporte-avance.component.ts
+++ FrontEnd/src/app/components/client/reporte-avance/reporte-avance.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ControlValueAccessor, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reporte-avance',
   templateUrl: './reporte-avance.component.html',
   styleUrls: ['./reporte-avance.component.css']
 })
 export class ReporteAvanceComponent implements OnInit {
-
+  form:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
