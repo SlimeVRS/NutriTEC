528dd5855c965131e5e8881ce5e7b62e790d65a5
03-Kevin <kejorola@gmail.com>
2021-11-14 13:53:08 -0600

    recipes model

--- FrontEnd/src/app/components/client/gestion-recetas/gestion-recetas/gestion-recetas.component.ts
+++ FrontEnd/src/app/components/client/gestion-recetas/gestion-recetas/gestion-recetas.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ControlValueAccessor, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-gestion-recetas',
   templateUrl: './gestion-recetas.component.html',
   styleUrls: ['./gestion-recetas.component.css']
 })
 export class GestionRecetasComponent implements OnInit {
-
+  form:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
