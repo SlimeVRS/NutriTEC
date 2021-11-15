1137da1d6fdd7d6be5a4ab4072d94a74764ed257
Marco Rivera <marco99eu@gmail.com>
2021-11-15 09:51:19 -0600

    Population edit

--- SQL Scritps/POPULATION.sql
+++ SQL Scritps/POPULATION.sql
@@ -1,11 +1,11 @@
-INSERT INTO patients
-VALUES(0, 'Paciente', 'Base', 'Para', 'FrontEND','1999-10-12', 0, 0, 0, 0, 'NC', 0,0,0,0,0,0)
-
 INSERT INTO admins
 VALUES(1548135, 'Marco', '', 'Rivera', 'Serrano', '11111111')
 
 INSERT INTO nutritionists
 VALUES(0, 'Nutricionista', 'Base', 'Para', 'FrontEND', '1999-10-12', 0, 0, 0, 'pfp',123,1,'direction')
+
+INSERT INTO patients
+VALUES(0, 'Paciente', 'Base', 'Para', 'FrontEND','1999-10-12', 0, 0, 0, 0, 'NC', 0,0,0,0,0,0)
 
 INSERT INTO foods
 VALUES(123456789, 'frijoles', '100 g', 1, 'A: 0%, C:2%', 2, 11, 241, 18, 101, 5, 0)
@@ -17,13 +17,7 @@ INSERT INTO foods
 VALUES(456789123, 'Huevo', '68 g', 10.9, 'A: 0%, C:0%', 0, 0, 0, 2.7, 180, 16.3, 0)
 
 INSERT INTO foods
-VALUES(123456789, 'Jamón de Pechuga de Pavo', '100 g', 2, 'A: 0%, C:0%', 0, 0, 980, 6, 101, 14, 0)
+VALUES(456123789, 'Jamón de Pechuga de Pavo', '100 g', 2, 'A: 0%, C:0%', 0, 0, 980, 6, 101, 14, 0)
 
 INSERT INTO foods
-VALUES(123456789, 'Pan Blanco', '22.7 g', 0, 'A: 0%, C:0%', 0, 0, 0, 10.4, 54, 2.4, 0)
-
-INSERT INTO food_plan
-VALUES('plan 1', 'papa', 'yogurt', 'arroz', 'sandwich', 'atún')
-
-INSERT INTO foods
-VALUES(785421587, 'Tu mama', '22.7 g', 0, 'A: 0%, C:0%', 0, 0, 0, 10.4, 54, 2.4, 1)
+VALUES(123789456, 'Pan Blanco', '22.7 g', 0, 'A: 0%, C:0%', 0, 0, 0, 10.4, 54, 2.4, 0)
